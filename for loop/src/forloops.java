439ae0c4adb3078806a8bdfab6e164c1bffc4829
kexin.yu <kexin.yu@MU-C1-19.mullen.pvt.k12.co.us>
2018-08-28 12:30:05 -0600

    sixhcommit

--- for loop/src/forloops.java
+++ for loop/src/forloops.java
@@ -1,20 +1,40 @@
+import java.util.Scanner;
 
 public class forloops {
 
 	public static void main(String[] args){
 		showGeniusOne();
+		showGeniusTwo();
+		
 	}	
+		private static void showGeniusTwo()
+		{
+			Scanner number = new Scanner(System.in);
+	    	System.out.println("Choose a upper bound");
+	    	int upper = number.nextInt();
+	    	System.out.println("Choose a lower bound");
+	    	int lower = number.nextInt();
+	    for(int a=lower; a<upper; a+=1)
+	    	{
+	    		
+	    	}
+		}
 		public static void showGeniusOne() 
 		{
 		
-		int sum=0;
-		   for (int n=5; n<=100; n+=5 ){
-			sum+=n;
-			
-		}
-		   System.out.print(sum);
-		   
+			int sum=0;
+			   for (int n=5; n<=100; n+=5 ){
+				sum+=n;
+				
+			   }
+			   System.out.print(sum);
+			   
 
-	}
+		}
+		
+		
+		
+		
+		
 
 }
